71d8d9ed43590bd9e4d935273b60b48e3eac3043
maliksdf <142108957+maliksdf@users.noreply.github.com>
2023-09-06 11:56:10 -0700

    Add files via upload

---
+++ programm(all datatype used).cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main() 
+{  
+string name[]="ayesha";
+int age=22;
+char selection= 'A';
+string subject[]="programming fundamental";
+int marks=90;
+float avg=9.60;
+
+cout<<"-----student detail----";
+cout<<"name="<<name<<"\n";
+cout<<"age="<<age<<"\n";
+cout<<"selection="<<selection<<"\n";
+cout<<"subject="<<subject<<"\n";
+cout<<"marks="<<marks<<"\n";
+cout<<"avg="<<avg<<"\n";
+return 0;
+}
