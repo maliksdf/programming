d6bfd7e6b424a3ee30ca340b5c42a4d7edd7cb63
fundamental programming//Ayesha Amin <142108957+maliksdf@users.noreply.github.com>
2023-09-16 14:08:23 -0700

    Add files via upload

---
+++ program 26(  print counting & their sum).cpp
@@ -0,0 +1,15 @@
+//Title:Write a program that displays first five numbers and their sum using while loop.
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n=1,sum=0;
+	while(n<=5)
+	{
+		cout<<n<<"\n";
+		sum=sum+n;
+		n++;
+	}
+	cout<<"sum"<<sum<<"\n";
+	return 0;
+}
