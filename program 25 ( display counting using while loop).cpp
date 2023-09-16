53121950290b4e62d9f522f742368298ab269103
fundamental programming//Ayesha Amin <142108957+maliksdf@users.noreply.github.com>
2023-09-16 13:57:55 -0700

    Add files via upload

---
+++ program 25 ( display counting using while loop).cpp
@@ -0,0 +1,13 @@
+//Title:Write a program that displays counting from 1 to 10 using while loop.
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n=1;
+	while(n<=10)
+	{
+		cout<<"counting"<<n<<"\n";
+		n++;
+	}
+	
+}
