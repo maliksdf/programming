52d4dec1390283e2cfd07c8e22305ae9a10514dc
fundamental programming//Ayesha Amin <142108957+maliksdf@users.noreply.github.com>
2023-09-23 02:53:54 -0700

    Add files via upload

---
+++ program 36cpp.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int c=1,p=1;
+    int n;
+    cout<<"enter the num" "\n";
+    cin>>n;
+    while(c<=n){
+                p=p*c;
+                c++;
+                }
+                cout<<"fact of n" <<p<< "\n";
+                return 0;
+   }
+                
